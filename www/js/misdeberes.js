4c486b903721c746e4d00d9c27f0d0357383f2df
CrisNaDeveloper <47522942+CrisNaDeveloper@users.noreply.github.com>
2020-02-14 14:03:36 +0100

    Add files via upload

--- www/js/misdeberes.js
+++ www/js/misdeberes.js
@@ -39,7 +39,7 @@ firebase.auth().languageCode = 'es_es';
 	}
 	
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
+firebase.auth().signInWithRedirect(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
